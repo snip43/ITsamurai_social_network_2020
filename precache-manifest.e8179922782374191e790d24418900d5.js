7dcc681c5982695c799f43ebad32bfd08101028c
snip43 <rko43@mail.ru>
2020-02-26 16:55:23 +0300

    copy build files

---
+++ precache-manifest.e8179922782374191e790d24418900d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda263e0ab751e1998bd8e8fd9dd2d7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45f9d30e92eb029342c0",
+    "url": "/static/css/main.ca26e55e.chunk.css"
+  },
+  {
+    "revision": "3c42acc63c9064071771",
+    "url": "/static/js/2.c4d70dd8.chunk.js"
+  },
+  {
+    "revision": "45f9d30e92eb029342c0",
+    "url": "/static/js/main.44949fd4.chunk.js"
+  },
+  {
+    "revision": "4be71d9aa6f0d07d0bba",
+    "url": "/static/js/runtime-main.8e2ff40c.js"
+  },
+  {
+    "revision": "ee86bb6dc80124a806ccde6a29a57cea",
+    "url": "/static/media/photoDefault.ee86bb6d.jpg"
+  }
+]);
