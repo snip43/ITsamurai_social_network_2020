cbcb36fb80e621ced311adc6fd5f280b81b74d05
snip43 <rko43@mail.ru>
2020-02-26 17:43:55 +0300

    Updates

---
+++ precache-manifest.b1c766cc585e6892210a1ef8a599d9dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848aa926a822f3197bfcf7dfda0b581a",
+    "url": "/ITsamurai_social_network_2020/index.html"
+  },
+  {
+    "revision": "45f9d30e92eb029342c0",
+    "url": "/ITsamurai_social_network_2020/static/css/main.ca26e55e.chunk.css"
+  },
+  {
+    "revision": "3c42acc63c9064071771",
+    "url": "/ITsamurai_social_network_2020/static/js/2.c4d70dd8.chunk.js"
+  },
+  {
+    "revision": "45f9d30e92eb029342c0",
+    "url": "/ITsamurai_social_network_2020/static/js/main.44949fd4.chunk.js"
+  },
+  {
+    "revision": "6193aebff5cf7a3b4b01",
+    "url": "/ITsamurai_social_network_2020/static/js/runtime-main.f1cb4e00.js"
+  },
+  {
+    "revision": "ee86bb6dc80124a806ccde6a29a57cea",
+    "url": "/ITsamurai_social_network_2020/static/media/photoDefault.ee86bb6d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8179922782374191e790d24418900d5.js"
+  "/ITsamurai_social_network_2020/precache-manifest.b1c766cc585e6892210a1ef8a599d9dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ITsamurai_social_network_2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
